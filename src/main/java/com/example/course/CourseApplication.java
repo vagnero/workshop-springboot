ef09ccfcbece6e3927bd9e461afe58fb90103084
vagnero <vagnerimperador16@gmail.com>
2024-02-29 22:43:03 -0300

    Segundo commit, criação do DB

--- src/main/java/com/example/course/CourseApplication.java
+++ src/main/java/com/example/course/CourseApplication.java
@@ -3,6 +3,8 @@ package com.example.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.course.entities.User;
+
 @SpringBootApplication
 public class CourseApplication {
 
